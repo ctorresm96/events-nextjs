9158f44b199219a0486afb210c7fbded98b7a99e
ctorresm96 <carlostm320@gmail.com>
2022-04-24 17:20:12 -0500

    first commit

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import { getFeaturedEvents } from "../helpers/api-util";
+import EventList from "../components/events/event-list";
+
+function HomePage(props) {
+  return (
+    <div>
+      <EventList items={props.featuredEvents} />
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
+  return {
+    props: {
+      featuredEvents,
+    },
+  };
+}
+
+export default HomePage;
